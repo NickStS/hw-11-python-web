c54da883fa51b8e2a63aade3737dca58bc96841e
danger_videograph <78549442+NickStS@users.noreply.github.com>
2023-09-22 23:27:09 +0300

    commit

---
+++ app/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, models, schemas, database
+
+app = FastAPI()
+
+database.Base.metadata.create_all(bind=database.engine)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
+    return crud.create_contact(db, contact)
+
+@app.get("/contacts/", response_model=list[schemas.Contact])
+def list_contacts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_contacts(db, skip, limit)
+
+@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.get_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.put("/contacts/{contact_id}", response_model=schemas.Contact)
+def update_contact(contact_id: int, contact: schemas.ContactUpdate, db: Session = Depends(get_db)):
+    db_contact = crud.update_contact(db, contact_id, contact)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+@app.delete("/contacts/{contact_id}", response_model=schemas.Contact)
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    db_contact = crud.delete_contact(db, contact_id)
+    if db_contact is None:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    return db_contact
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
